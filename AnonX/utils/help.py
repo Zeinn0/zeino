85c22302f93bc72610f50b634ab5e50c0f1c7a98
Zeinn0 <127055306+Zeinn0@users.noreply.github.com>
2023-05-10 17:08:04 +0300

    Add files via upload

---
+++ AnonX/utils/help.py
@@ -0,0 +1,112 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def help_pannel(_, START: Union[bool, int] = None):
+    first = [
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        )
+    ]
+    second = [
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"settingsback_helper",
+        ),
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        ),
+    ]
+    mark = second if START else first
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="المشرف",
+                    callback_data="help_callback hb1",
+                ),
+                InlineKeyboardButton(
+                    text="منشئ",
+                    callback_data="help_callback hb2",
+                ),
+                InlineKeyboardButton(
+                    text="قايمه المحظورين",
+                    callback_data="help_callback hb3",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="الإذاعة",
+                    callback_data="help_callback hb4",
+                ),
+                InlineKeyboardButton(
+                    text="الحظر",
+                    callback_data="help_callback hb12",
+                ),
+                InlineKeyboardButton(
+                    text="المطورين",
+                    callback_data="help_callback hb5",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="بينج",
+                    callback_data="help_callback hb7",
+                ),
+                InlineKeyboardButton(
+                    text="التشغيل",
+                    callback_data="help_callback hb8",
+                ),
+                InlineKeyboardButton(
+                    text="قايمه التشغيل",
+                    callback_data="help_callback hb6",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="المكالمات",
+                    callback_data="help_callback hb10",
+                ),
+                InlineKeyboardButton(
+                    text="الاسترت",
+                    callback_data="help_callback hb11",
+                ),
+                InlineKeyboardButton(
+                    text="البوت",
+                    callback_data="help_callback hb9",
+                ),
+            ],
+            mark,
+        ]
+    )
+    return upl
+
+
+def help_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"settings_back_helper",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"], callback_data=f"إغلاق"
+                )
+            ]
+        ]
+    )
+    return upl
+
+
+def private_help_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🗒 الاوامر",
+                callback_data="settings_back_helper",
+            ),
+        ],
+    ]
+    return buttons
